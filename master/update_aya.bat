598ab935cc125b8d6c8a055e09220b14693951c1
steve green <steve02081504@EDEN.fukwold>
2021-06-30 20:33:23 +0800

    Update in 7.1-7.3
    BUG fix
    saori体积压缩&dic界面修正

---
+++ master/update_aya.bat
@@ -0,0 +1,9 @@
+﻿@echo off
+rem 此脚本只适用于Taromati2，除非你给你的人格也配备上ghost_terminal和reload快捷指令
+cd /d %~dp0
+ren aya.dll tmp
+copy %1 aya.dll
+.\saori\ghost_terminal.exe -c reload
+timeout /t 1
+del tmp
+@echo on
