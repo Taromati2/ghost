4dcca19806093d88048c20e475063d98aa4feade
steve02081504 <steve02081504@EDEN.fukwold>
2021-07-10 13:36:07 +0800

    Update core

--- master/shiori/update_aya.bat
+++ master/shiori/update_aya.bat
@@ -3,7 +3,7 @@ rem 此脚本只适用于Taromati2，除非你给你的人格也配备上ghost_t
 cd /d %~dp0
 ren aya.dll tmp
 copy %1 aya.dll
-.\saori\ghost_terminal.exe -c reload
-timeout /t 1
+..\saori\ghost_terminal.exe -c reload
+timeout /t 7
 del tmp
 @echo on
